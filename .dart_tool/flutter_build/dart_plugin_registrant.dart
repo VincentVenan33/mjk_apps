25f2ddba397458f33ff430ad6d751bf0a41ed852
VincentVenan33 <92298629+VincentVenan33@users.noreply.github.com>
2024-02-07 08:14:10 +0700

    tracking pengiriman maps blm sukses

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,12 +6,14 @@
 // @dart = 3.0
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:image_picker_android/image_picker_android.dart';
 import 'package:local_auth_android/local_auth_android.dart';
 import 'package:path_provider_android/path_provider_android.dart';
 import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:url_launcher_android/url_launcher_android.dart';
+import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
 import 'package:local_auth_ios/local_auth_ios.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
@@ -50,6 +52,15 @@ class _PluginRegistrant {
   @pragma('vm:entry-point')
   static void register() {
     if (Platform.isAndroid) {
+      try {
+        GoogleMapsFlutterAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         ImagePickerAndroid.registerWith();
       } catch (err) {
@@ -105,6 +116,15 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isIOS) {
+      try {
+        GoogleMapsFlutterIOS.registerWith();
+      } catch (err) {
+        print(
+          '`google_maps_flutter_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         ImagePickerIOS.registerWith();
       } catch (err) {
